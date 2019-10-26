fe89e33858bad9353ae75b0c23f57d434d28ab56
TIE <riccardoarona>
2019-10-26 15:01:05 +0200

    UPD: video driver and ssh

--- raspcam/app/start.sh
+++ raspcam/app/start.sh
@@ -1,3 +1,16 @@
+# Start sshd if we don't use the init system
+if [ "$INITSYSTEM" != "on" ]; then
+    echo "starting SSH ..."
+    /usr/sbin/sshd -p 22 &
+fi
+
+echo "rendering image"
+# cat /usr/src/app/images/image.raw > /dev/fb1
+fbi -d /dev/fb0 -T 1 -noverbose -a ./client/favicon.png
+
+sleep 999
+
 # Notifying service start
 echo "starting service ..."
 python3 /usr/src/app/server.py
+
